20142db0ce70002052eb04079ff8f18620f8c33b
Sachin Kumar <93379945+B19EE075@users.noreply.github.com>
2022-10-07 21:25:20 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define inf 1e9
+#define mod 1e9+7
+
+/* freopen("input.txt","r",stdin); */
+/* freopen("output.txt","w",stdout); */
+
+/*========= Actual CODE STARTS FROM ==========HERE*/
+
+
+int solve()
+{
+	int n ; cin>>n;
+}
+
+signed main()
+{
+	int t;cin>>t;
+	while(t--)
+	{
+		cout<<solve()<<endl;
+	}
+}
